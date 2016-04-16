ab9325a351884043281356752eabe7df4ef3de82
David Urbina <durbina1991@gmail.com>
2016-04-15 18:07:46 -0700

    Clean-up, Organize in Object, Add Persistence

--- main.js
+++ main.js
@@ -1,25 +1,91 @@
 
-/*function contactProfile (name, email, address, number) {
-    this.name = name;
-    this.email = email;
-    this.address = address;
-    this.phoneNumber = number;
-};*/
 
-let addContact = function () {
-    var saveContact = $("#saveChanges");
-    saveContact.click(function () {
-        //Use an object to hold the data
-       // let newContact = {};
-        let $name = $("#newFullName").val();
-        let $email = $("#newEmail").val();
-        let $address = $("#newAddress").val();
-        let $number = $("#newNumber").val();
-        createNewEntry($name, $email, $address, $number);
-        $('#myModal').modal('hide');
 
-    });
+
+
+var operations = {
+
+    //obtain the array of contact-objects on local storage
+    getNames : function () {
+        try {
+            var names = JSON.parse(localStorage.contactEntries);
+        } catch (err) {
+            var names = [];
+        }
+        return names;
+    },
+
+    //writes to the local storage
+    writeNamesLocal : function (arrayOfObjects) {
+        localStorage.contactEntries = JSON.stringify(arrayOfObjects);
+    },
+
+    //to generate a new JSON object for the contact
+    ContactProfile : function (name, email, address, number) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = number;
+    },
+
+    //response to submit button with filled-out information
+    submitNewContact : function () {
+        var saveContactButton = $("#saveChanges");
+        saveContactButton.click(function () {
+            let $name = $("#newFullName").val();
+            let $email = $("#newEmail").val();
+            let $address = $("#newAddress").val();
+            let $number = $("#newNumber").val();
+            //create li element
+            operations.createEntryElement($name, $email, $address, $number);
+            //create JSON object
+            operations.createNewLocalEntry($name, $email, $address, $number);
+            $('#myModal').modal('hide');
+        });
+    },
+
+    //add entry to array of contact-objects on local storage;
+    createNewLocalEntry : function (name, email, address, number) {
+        var newJSONObject = new operations.ContactProfile(name, email, address, number);
+        var storedContacts = operations.getNames();
+        storedContacts.push(newJSONObject);
+        operations.writeNamesLocal(storedContacts);
+    },
+
+    //create li element for the actual address book
+    createEntryElement : function (name, email, address, phone) {
+        var addressBook = $(".addressBook");
+        var $li = $("<li ripple>");
+        //have img accept a url
+        var $image = $("<img>").addClass("item-icon").attr("src", "http://dev.alurosu.com/bobo/chat/data/img/admin/default.png");
+        var $firstSpan = $("<span>").addClass("item-text").text(name);
+        var $secondSpan = $("<span>").addClass("secondary-text").text(email);
+
+        $li.append($image);
+        $li.append($firstSpan);
+        $firstSpan.append($secondSpan);
+
+        addressBook.append($li);
+
+    },
+
+    //render the contact list on load
+    renderContactsOnLoad : function () {
+        var contacts = operations.getNames();
+        contacts.forEach(function (item) {
+            operations.createEntryElement(item.name, item.email, item. address, item.phoneNumber);
+
+        });
+    }
+
 };
+
+
+
+
+
+//var testObject = new operations.ContactProfile("stuff", "here", "and", "there");
+
 
 //create a div to populate with further contact info
 /*let populateContactInfo = function (contact) {
@@ -44,27 +110,11 @@ let addContact = function () {
 
 
 
-let createNewEntry = function (name, email, address, phone) {
-    var addressBook = $(".addressBook");
-    var $li = $("<li ripple>")
-    var $image = $("<img>").addClass("item-icon").attr("src", "http://dev.alurosu.com/bobo/chat/data/img/admin/default.png");
-    var $firstSpan = $("<span>").addClass("item-text").text(name);
-    var $secondSpan = $("<span>").addClass("secondary-text").text(email);
-
-    $li.append($image);
-    $li.append($firstSpan);
-    $firstSpan.append($secondSpan);
-
-
-    addressBook.append($li);
-
-    console.log($li);
-};
 
 let initialize = function () {
-    addContact();
-   // populateContactInfo();
-}
+    operations.submitNewContact();
+    operations.renderContactsOnLoad();
+};
 
 
 
---
+++ test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by david on 4/15/16.
+ */
+var titleCase = function (string) {
+    var results = [];
+    var words = string.split(" ");
+      
+};
+
+var test = "How can mirrors be real if our eyes";
+
+console.log(titleCase(test));
